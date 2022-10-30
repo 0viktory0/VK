045fade43c311f70fafd932985fb8d472650057d
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2022-10-31 00:14:54 +0300

    delete unnecessary variable and fixed functions

--- main.py
+++ main.py
@@ -10,17 +10,13 @@ def get_random_img():
     num_img = main_response.json()['num']
     img_number = randint(0, num_img)
     response = requests.get(f'https://xkcd.com/{img_number}/info.0.json')
-    response.raise_for_status()
-    return response.json()
-
-
-def download_image(response):
-    image = requests.get(response['img'])
+    image = requests.get(response.json()['img'])
     image.raise_for_status()
+    img_comment = response.json()['alt']
     img_name = 'comic.png'
     with open(img_name, 'wb') as file:
         file.write(image.content)
-    return img_name
+    return img_name, img_comment
 
 
 def get_upload_url(access_token, group_id):
@@ -33,43 +29,42 @@ def get_upload_url(access_token, group_id):
     return upload_url
 
 
-def upload_img_to_server(img_name, access_token, group_id):
+def upload_img_to_server(img_name, url_for_upload):
     with open(img_name, 'rb') as file:
         files = {'photo': file}
-        url_for_upload = get_upload_url(access_token, group_id)
         response = requests.post(url_for_upload, files=files)
-        response.raise_for_status()
-        server = response.json()['server']
-        hash = response.json()['hash']
-        photo = response.json()['photo']
-        return server, hash, photo
+    response.raise_for_status()
+    saved_img = response.json()
+    server = saved_img['server']
+    hash_img = saved_img['hash']
+    photo = saved_img['photo']
+    return server, hash_img, photo
 
 
-def upload_img_to_wall(group_id, img_name, access_token, api_version):
+def upload_img_to_wall(group_id, access_token, server, hash_img, photo):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
-    server, hash, photo = upload_img_to_server(img_name, access_token, group_id)
     payload = {'group_id': group_id,
                'server': server,
                'photo': photo,
-               'hash': hash,
+               'hash': hash_img,
                'access_token': access_token,
-               'v': api_version}
+               'v': '5.131'}
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    owner_id = response.json().get('response')[0]['owner_id']
-    media_id = response.json().get('response')[0]['id']
+    uploaded_img = response.json()
+    owner_id = uploaded_img.get('response')[0]['owner_id']
+    media_id = uploaded_img.get('response')[0]['id']
     return owner_id, media_id
 
 
-def publish_img_to_wall(group_id, img_comment, access_token, api_version):
+def publish_img_to_wall(group_id, img_comment, access_token, owner_id, media_id):
     url = f"https://api.vk.com/method/wall.post"
-    owner_id, media_id = upload_img_to_wall(group_id, access_token, api_version)
     payload = {'owner_id': f'-{group_id}',
                'from_group': 1,
                'attachments': f'photo{owner_id}_{media_id}',
                'message': img_comment,
                'access_token': access_token,
-               'v': api_version}
+               'v': '5.131'}
     response = requests.post(url, params=payload)
     response.raise_for_status()
 
@@ -78,13 +73,17 @@ def main():
     load_dotenv()
     access_token = os.environ['VK_TOKEN']
     group_id = os.environ['GROUP_ID']
-    api_version = os.environ['API_VERSION']
 
-    img_response = get_random_img()
-    img_comment = img_response['alt']
-    img_name = download_image(img_response)
-    publish_img_to_wall(group_id, img_comment, access_token, api_version)
-    os.remove(img_name)
+    img_name, img_comment = get_random_img()
+    try:
+        url_for_upload = get_upload_url(access_token, group_id)
+        server, hash_img, photo = upload_img_to_server(img_name, url_for_upload)
+        owner_id, media_id = upload_img_to_wall(group_id, access_token, server, hash_img, photo)
+        publish_img_to_wall(group_id, img_comment, access_token, owner_id, media_id)
+    except requests.exceptions.HTTPError:
+        print(traceback.format_exc())
+    finally:
+        os.remove(img_name)
 
 
 if __name__ == '__main__':
